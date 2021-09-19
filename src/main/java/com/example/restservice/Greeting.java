298c9a227fed4c1cdd4b9b3be97f07a18d886d67
Thiru Gayam <tgayam@thirus-air.lan>
2021-09-19 13:47:31 -0400

    filechanged

--- src/main/java/com/example/restservice/Greeting.java
+++ src/main/java/com/example/restservice/Greeting.java
@@ -4,7 +4,7 @@ public class Greeting {
     private final long id;
     private final String content;
 
-    public Greeting(long id, String content) {
+    public Greeting(long id, String content) {git remote add origin https://github.com/gayam2021/demo-greeting.git
         this.id = id;
         this.content = content;
     }
@@ -16,4 +16,5 @@ public class Greeting {
     public String getContent() {
         return content;
     }
+
 }
